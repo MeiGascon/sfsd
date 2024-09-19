11a41b4bd819d375c30ae23426241da904858720
MeiGascon <meigasbur@gmail.com>
2024-09-19 17:28:59 +0200

    He añadido la función say_bye y otras pequeñas modificaciones. para el archivo prueba.py

--- prueba.py
+++ prueba.py
@@ -14,8 +14,16 @@ print("Hoy aldar nos ha ayudado a instalar python.")
 #hola esto es una prueba
 
 #FUNCIONES
-
 def say_hello ():
-    print("estoy ejecutando la función llamada say_hello")
+    print("Estoy ejecutando la función llamada say_hello.") #al añadir texto a una línea ya existente, aparece una línea azul
 
 say_hello()
+
+
+#con esta nueva función, como hemos AÑADIDO CÓDIGO, se observa una línea verde
+def say_bye ():
+    print("ejecutando la función say_bay")
+
+say_bye()
+
+# si eliminamos una línea, se observa una flecha roja. 
