63d603542a98f5224385302991180bd9bde98f38
MeiGascon <meigasbur@gmail.com>
2024-09-19 17:14:40 +0200

    First commit: using git with visual studio code

---
+++ prueba.py
@@ -0,0 +1,21 @@
+print("hello")
+print("hello world")
+
+x = 1
+print(x)
+
+y = 2
+
+total = x + y
+print(total)
+
+
+print("Hoy aldar nos ha ayudado a instalar python.")
+#hola esto es una prueba
+
+#FUNCIONES
+
+def say_hello ():
+    print("estoy ejecutando la función llamada say_hello")
+
+say_hello()
